7207174cc272aab023f2f244a9a86b764e2b195f
uzapy <uzapy@outlook.com>
2016-12-30 23:27:10 +0100

    Dokumente

--- Schneedetektion/Schneedetektion.Data/Queries/coverageOfCategories.sql
+++ Schneedetektion/Schneedetektion.Data/Queries/coverageOfCategories.sql
@@ -3,7 +3,3 @@ from Combined_Statistics, Polygons
 where Combined_Statistics.Polygon_ID = Polygons.ID
 group by [CombinationMethod], [Polygons].CameraName
 order by [Polygons].CameraName
-
-select top(100) len([Statistics].[GreenHistogram]) as [length] from [Statistics]
-
-select top(100) len([Combined_Statistics].[Images]) as [length] from [Combined_Statistics]
